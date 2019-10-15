2776c196888a6d6fa22dd593deb386e70cb6476e
Shiyam23 <39777757+Shiyam23@users.noreply.github.com>
2019-10-15 12:58:28 +0200

    refactored transaction

--- lib/models/Transaction.dart
+++ lib/models/Transaction.dart
@@ -4,38 +4,38 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/text.dart';
 import 'package:intl/intl.dart';
 import 'package:project_ez_finance/components/CategoryIcon.dart';
+import 'package:project_ez_finance/models/Category.dart';
 import 'package:project_ez_finance/models/SelectableTile.dart';
 
-class Transaction with SelectableTile {
+class Transaction implements SelectableTile {
   String name;
   DateTime date;
   CategoryIcon icon;
   String amount;
   bool isExpense;
-
-  Transaction(
-      {@required this.name,
-      @required this.date,
-      @required this.icon,
-      @required this.amount,
-      @required this.isExpense});
-
-  @override
-  // TODO: implement rightText
-  Text get rightText => Text(amount.toString(),
-      style: TextStyle(
-          fontSize: 18,
-          color: isExpense ? Colors.red : Colors.green,
-          fontWeight: FontWeight.w500));
-
-  @override
-  // TODO: implement secondaryTitle
-  Text get secondaryTitle => Text(
-        "Standardausgabe\n" + DateFormat("dd.MM.yyyy").format(date),
-        style: TextStyle(fontSize: 13),
-      );
-
-  @override
-  // TODO: implement title
-  Text get title => Text(name);
-}
+  TransactionCategory category;
+  
+    Transaction(
+        {@required this.name,
+        @required this.category,
+        @required this.date,
+        @required this.icon,
+        @required this.amount,
+        @required this.isExpense});
+  
+    @override
+    Text get rightText => Text(amount.toString(),
+        style: TextStyle(
+            fontSize: 18,
+            color: isExpense ? Colors.red : Colors.green,
+            fontWeight: FontWeight.w500));
+  
+    @override
+    Text get secondaryTitle => Text(
+          "${this.category.name}\n" + DateFormat("dd.MM.yyyy").format(date),
+          style: TextStyle(fontSize: 13),
+        );
+  
+    @override
+    Text get title => Text(name);
+  }

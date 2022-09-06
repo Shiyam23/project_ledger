b18406ab5c3fd9c6ef4411f20778c415a1edc155
Shiyam23 <39777757+Shiyam23@users.noreply.github.com>
2022-09-06 21:04:55 +0200

    Remove unnecessary imports

--- lib/screens/new/income_expense/newTextFieldController/NewRepetitionDialog.dart
+++ lib/screens/new/income_expense/newTextFieldController/NewRepetitionDialog.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:intl/intl.dart';
 import 'package:project_ez_finance/components/dialogs/ResponseDialog.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
 import 'package:project_ez_finance/models/Repetition.dart';
 import 'package:project_ez_finance/services/DateTimeFormatter.dart';
 
